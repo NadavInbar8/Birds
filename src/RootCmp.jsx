5d51c3b5f1fa446b630964d43168badc7d3cc52a
Nadav Inbar <nadav.inbar@enwize-tw.com>
2023-03-29 13:54:48 +0300

    added hello world

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -4,6 +4,7 @@ import { Homepage } from './pages/Homepage';
 export function RootCmp() {
   return (
     <main className='main-app-layout flex'>
+      <h1>Hello world!</h1>
       <Homepage />
     </main>
   );
